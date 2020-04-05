40b6e056e12e5fb7deb7b3adf799089c57891b77
tahziaS2 <62951753+tahziaS2@users.noreply.github.com>
2020-04-06 00:39:29 +0200

    fix(stream): Fix eslint errors + add styling (#7)
    Co-authored-by: Ola Gasidlo <ola.gasidlo@sinnerschrader.com>
    Co-authored-by: Taha Zia <taha.zia@sinnerschrader.com>

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const OLAPIC_API_KEY = '61cf3b42fe12c7ade947540560487fda268e47d6b7556db3dee1045b0d1d09e0';
+export const OLAPIC_URL = 'https://photorankapi-a.akamaihd.net/streams/2208072494?version=v2.2';
