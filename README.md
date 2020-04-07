0e588b2059c4477b310b0afb7d33a7d8ebfc64a3
Tamara Kis <kis.tamara91@gmail.com>
2020-04-07 11:41:57 +0200

    Kisstance/coskis 9/slider glidejs (#6)
    * feat(slider): added slider 
    
    Co-authored-by: tamara.kis <tamara.kis@sinnerschrader.com>
    Co-authored-by: Ola Gasidlo <o.gasidlo@gmail.com>

--- README.md
+++ README.md
@@ -18,4 +18,11 @@ All options:
 npm run lint:css
 npm run lint:js
 npm run lint:all
+npm run lint:css:fix
+npm run lint:js:fix
+```
+## clean SVG
+
+```
+npm run clean:svg
 ```
---
+++ src/js/main.js
@@ -0,0 +1,49 @@
+import { OLAPIC_API_KEY, OLAPIC_URL } from './config.js';
+
+function returnImageTags(arr) {
+  const media = arr.data._embedded.media;
+  let tags = '';
+  for (let i = 0; i < 4; i++) {
+    tags = tags + (
+      '<img class="olapic_img" alt="kisstance instagram post number '
+        + (i+1)
+        + '" src="'
+        + media[i].images.mobile
+        + '"> <br />'
+    );
+  }
+  document.getElementById('js-olapic_stream').innerHTML = tags + document.getElementById('js-olapic_stream').innerHTML;
+}
+
+function getImages(url) {
+  const request = new XMLHttpRequest();
+
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      const myArr = JSON.parse(this.responseText);
+      returnImageTags(myArr);
+    }
+  };
+  request.open('GET', url, true);
+  request.send();
+}
+
+function getImageUrls() {
+  const httpRequest = new XMLHttpRequest();
+  const url = OLAPIC_URL + '&auth_token=' + OLAPIC_API_KEY;
+
+  httpRequest.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      const myArr = JSON.parse(this.responseText);
+      const imageUrl = 'https:' + myArr.data._embedded['media:recent']._links.self.href;
+      getImages(imageUrl);
+    }
+  };
+  httpRequest.open('GET', url, true);
+  httpRequest.send();
+}
+
+getImageUrls();
+
+// mount Glide.js
+new Glide('.glide').mount()
