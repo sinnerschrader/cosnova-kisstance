7f049fb298048c5893a04f6cede97639e67e9d6f
Ola Gasidlo <o.gasidlo@gmail.com>
2020-04-12 09:28:08 +0200

    Test master (#28)
    * feat(counter): get posts from spreadsheet
    * chore(app): update package-lock
    * TEST_DEPLOY... WILL BE REVERTED
    
    Co-authored-by: Taha Zia <taha.zia@external.sinnerschrader.com>
    Co-authored-by: Ola Gasidlo <ola.gasidlo@sinnerschrader.com>

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,5 @@
 export const OLAPIC_API_KEY = '61cf3b42fe12c7ade947540560487fda268e47d6b7556db3dee1045b0d1d09e0';
 export const OLAPIC_URL = 'https://photorankapi-a.akamaihd.net/streams/2208072494?version=v2.2';
+export const SHEET_ID = '1cB66BKBUY5QLX4ibr6sn8QVAXiJI6dBAKBgKKZlI84w';
+export const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+export const SHEET_NAME = 'Sheet1';
